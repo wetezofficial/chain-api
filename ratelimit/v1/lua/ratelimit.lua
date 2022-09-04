19ce2d44087c78b3138a2913c543596eb797bd4c
Merlin <merlin@wetez.io>
2022-09-04 16:19:01 +0800

    feat(*.lua): ratelimit.lua add total_chain_quota_key & total_chain_quota_key

--- ratelimit/v1/lua/ratelimit.lua
+++ ratelimit/v1/lua/ratelimit.lua
@@ -1,9 +1,9 @@
-local api_key     	= KEYS[1]
+local api_key = KEYS[1]
 
-local chain_id 		= ARGV[1]
-local day	   		= ARGV[2]
-local n	   		    = tonumber(ARGV[3])
-local revert	    = ARGV[4]
+local chain_id = ARGV[1]
+local day = ARGV[2]
+local n = tonumber(ARGV[3])
+local revert = ARGV[4]
 
 local sec_quota_key = "q:s:" .. chain_id .. ":" .. api_key
 local day_quota_key = "q:d:" .. chain_id .. ":" .. api_key
@@ -13,21 +13,21 @@ local day_rate_limit_key = "d:" .. chain_id .. ":" .. api_key .. ":" .. day
 
 local sec_quota = tonumber(redis.call("GET", sec_quota_key))
 if not sec_quota then
-  return -1
+    return -1
 end
 
 local current = tonumber(redis.call("INCRBY", sec_rate_limit_key, n))
 if current == n then
-    redis.call("EXPIRE",sec_rate_limit_key,1)
+    redis.call("EXPIRE", sec_rate_limit_key, n)
 end
 
 if current > sec_quota then
-  return -2
+    return -2
 end
 
 local day_quota = tonumber(redis.call("GET", day_quota_key))
 if not day_quota then
-  return -1
+    return -1
 end
 
 current = tonumber(redis.call("INCRBY", day_rate_limit_key, n))
@@ -35,11 +35,34 @@ if current == n then
     redis.call("EXPIRE", day_rate_limit_key, 129600)
 end
 
+-- day chain quota num key
+local day_chain_quota_key = "q:cd:" .. day .. ":" .. chain_id
+-- total chain quota num key ct-> chain-total
+local total_chain_quota_key = "q:ct:" .. chain_id
+
+local day_chain_quota = tonumber(redis.call("GET", day_chain_quota_key))
+if not day_chain_quota
+then
+    -- add new key to save today chain quota
+    redis.call("SET", day_chain_quota_key, n)
+else
+    redis.call("INCRBY", day_chain_quota_key, n)
+end
+
+local total_chain_quota = tonumber(redis.call("GET", total_chain_quota_key))
+if not total_chain_quota
+then
+    -- add new key to save total chain quota
+    redis.call("SET", total_chain_quota_key, n)
+else
+    redis.call("INCRBY", total_chain_quota_key, n)
+end
+
 if current > day_quota then
-  if revert == "1" then
-    redis.call("DECRBY", day_rate_limit_key, current - day_quota)
-  end
-  return -3
+    if revert == "1" then
+        redis.call("DECRBY", day_rate_limit_key, current - day_quota)
+    end
+    return -3
 end
 
 return 1
